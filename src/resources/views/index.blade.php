c90a6d5000931f9041023934d19a4e273b8e9011
nishio <mayunishio0909@gmail.com>
2025-08-30 19:00:39 +0900

    CSS修正

--- src/resources/views/index.blade.php
+++ src/resources/views/index.blade.php
@@ -1,48 +1,69 @@
 @extends('layouts.app')
 
+@section('css')
+<link rel="stylesheet" href="{{ asset('css/index.css') }}">
+@endsection
+
 @section('content')
-<div class="container">
-    {{-- 作成完了メッセージ --}}
-    @if(session('success'))
-        <div class="alert alert-success">
-            {{ session('success') }}
-        </div>
-    @endif
+<div class="todo__alert">
+  <div class="todo__alert--success">
+    Todoを作成しました
+  </div>
+</div>
 
-    {{-- Todo 作成フォーム --}}
-    <form action="{{ route('todos.store') }}" method="POST" class="todo-form">
-        @csrf
-        <input type="text" name="title" placeholder="Todoを入力" required>
-        <button type="submit">作成</button>
-    </form>
-
-    {{-- Todo 一覧 --}}
-    <table class="todo-table">
-        <thead>
-            <tr>
-                <th>Todo</th>
-                <th>操作</th>
-            </tr>
-        </thead>
-        <tbody>
-            @foreach($todos as $todo)
-                <tr>
-                    <td>{{ $todo->title }}</td>
-                    <td>
-                        <form action="{{ route('todos.update', $todo->id) }}" method="POST" style="display:inline">
-                            @csrf
-                            @method('PUT')
-                            <button type="submit" class="btn-update">更新</button>
-                        </form>
-                        <form action="{{ route('todos.destroy', $todo->id) }}" method="POST" style="display:inline">
-                            @csrf
-                            @method('DELETE')
-                            <button type="submit" class="btn-delete">削除</button>
-                        </form>
-                    </td>
-                </tr>
-            @endforeach
-        </tbody>
+<div class="todo__content">
+  <form class="create-form">
+    <div class="create-form__item">
+      <input class="create-form__item-input" type="text" name="content">
+    </div>
+    <div class="create-form__button">
+      <button class="create-form__button-submit" type="submit">作成</button>
+    </div>
+  </form>
+  <div class="todo-table">
+    <table class="todo-table__inner">
+      <tr class="todo-table__row">
+        <th class="todo-table__header">Todo</th>
+      </tr>
+      <tr class="todo-table__row">
+        <td class="todo-table__item">
+          <form class="update-form">
+            <div class="update-form__item">
+              <input class="update-form__item-input" type="text" name="content" value="test">
+            </div>
+            <div class="update-form__button">
+              <button class="update-form__button-submit" type="submit">更新</button>
+            </div>
+          </form>
+        </td>
+        <td class="todo-table__item">
+          <form class="delete-form">
+            <div class="delete-form__button">
+              <button class="delete-form__button-submit" type="submit">削除</button>
+            </div>
+          </form>
+        </td>
+      </tr>
+      <tr class="todo-table__row">
+        <td class="todo-table__item">
+          <form class="update-form">
+            <div class="update-form__item">
+              <input class="update-form__item-input" type="text" name="content" value="test2">
+            </div>
+            <div class="update-form__button">
+              <button class="update-form__button-submit" type="submit">更新</button>
+            </div>
+          </form>
+        </td>
+        <td class="todo-table__item">
+          <form class="delete-form">
+            <div class="delete-form__button">
+              <button class="delete-form__button-submit" type="submit">削除</button>
+            </div>
+          </form>
+        </td>
+      </tr>
     </table>
+  </div>
 </div>
 @endsection
